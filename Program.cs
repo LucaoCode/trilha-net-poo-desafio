8a3831224692aaf5b4548d391989e18d24a67864
Lucas <lucas.lima@ucl.br>
2024-09-07 12:39:30 -0300

    Feature - Resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Instalando aplicativo no Nokia");
+            Smartphone n = new Nokia("27992048112","Nokia 7","B1234",8);
+            n.InstalarAplicativo("google play");
+
+            Console.WriteLine("Instalando aplicativo no Iphone");
+            Smartphone i = new Iphone("2733379520","Iphone 1","B1237",4);
+            i.InstalarAplicativo("Zap Zap");
+        }
+    }
+}
